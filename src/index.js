000cba0326050fcdc845050ec7688059c763901b
Harrsion <hstamps4@gmail.com>
2021-05-06 13:11:21 -0700

    feat(HomePage): added a form to the HomePage and imported into the app component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
